5af044939a8e5eb66437905658428e2234239f45
Andrew Wagner <andrew.wagner@esat.kuleuven.be>
2013-11-19 15:49:48 +0100

    Add toy cpp files that use the generated protobuf

---
+++ read.cpp
@@ -0,0 +1,21 @@
+#include <string>
+#include <iostream>
+#include <stdint.h>
+using namespace std;
+
+// Read a toy protobuf from a file.
+int main(int argc, char **argv) 
+{
+	Person person;
+	fstream in("person.pb", ios::in | ios::binary);
+	if (!person.ParseFromIstream(&in)) {
+		cerr << "Failed to parse person.pb." << endl;
+		exit(1);
+	}
+
+	cout << "ID: " << person.id() << endl;
+	cout << "name: " << person.name() << endl;
+	if (person.has_email()) {
+		cout << "e-mail: " << person.email() << endl;
+	}
+}
---
+++ write.cpp
@@ -0,0 +1,17 @@
+#include <string>
+#include <iostream>
+#include <stdint.h>
+using namespace std;
+
+// Write a toy protobuf to a file.
+int main(int argc, char **argv) 
+{
+	Person person;
+	person.set_id(123);
+	person.set_name("Bob");
+	person.set_email("bob@example.com");
+
+	fstream out("person.pb", ios::out | ios::binary | ios::trunc);
+	person.SerializeToOstream(&out);
+	out.close();
+}
